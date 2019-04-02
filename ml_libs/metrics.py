62a63df6f7c2ca46141973f4b8f4a10b81a5182f
Regis Pires Magalhães <regispiresmag@gmail.com>
2019-04-02 10:02:36 -0300

    update to Tutorial-Regressao-Linear.ipynb

--- ml_libs/metrics.py
+++ ml_libs/metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def mse(y, y_pred):
-    return np.sum(( y - y_pred ) ** 2)
+    return np.sum(( y - y_pred ) ** 2) / y.shape[0]
 
 def rmse(y, y_pred):
     return mse(y, y_pred) ** 0.5
